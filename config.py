1477b863f25e0217e32eb88cf297290763ed18e2
Nwtoomey <noahtoomeyBC@gmail.com>
2021-11-05 15:52:22 -0500

    Created web app for testing, figured out db querying

--- config.py
+++ config.py
@@ -1,29 +1,4 @@
 db_password = 'piechartspassword'
 
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config(object):
-    DEBUG = False
-    TESTING = False
-    CSRF_ENABLED = True
-    SECRET_KEY = 'piechartspassword'
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
-class ProductionConfig(Config):
-    DEBUG = False
-
-
-class StagingConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
-
-
-class DevelopmentConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
-
-
-class TestingConfig(Config):
-    TESTING = True
