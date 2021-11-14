a3c4e658770a1c683bbe2cbfe78e49c94d80a57a
Nwtoomey <noahtoomeyBC@gmail.com>
2021-11-14 16:12:02 -0600

    Testing Out Hiding/Displaying Divs on test.html

--- app/static/js/logic_machine_learn.js
+++ app/static/js/logic_machine_learn.js
@@ -1,8 +1,31 @@
-function mlShowHideFunc() {
-    var x = document.getElementById('myDIV');
+function prelimShowHideFunc() {
+    var x = document.getElementById('prelimDiv');
     if (x.style.display === 'none') {
       x.style.display = 'block';
     } else {
       x.style.display = 'none';
     }
   }
+
+function normalizehowHideFunc() {
+  var x = document.getElementById('normalizeDIV');
+  if (x.style.display === 'none') {
+    x.style.display = 'block';
+  } else {
+    x.style.display = 'none';
+  }
+}
+
+$("#postitemlocation").select2()
+
+$("#clickme1").click(function() {
+  $("#postitemlocation").select2("open");
+  $("#clickme1").hide();
+  $("#clickme2").show();
+});
+
+$("#clickme2").click(function() {
+  $("#postitemlocation").select2("close");
+  $("#clickme1").show();
+  $("#clickme2").hide();
+});
